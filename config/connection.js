3848608de6f5e1a43d560874675eeee15f0a4cf3
Dipinoar <113305509+Dipinoar@users.noreply.github.com>
2023-07-04 19:24:06 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
-
+/*
 const sequelize = new Sequelize('ecommerce5', 'postgres', '123456', {
     host: 'localhost',
     dialect: 'postgres'
 
 });
+*/
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env
+
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}&ssl=true`;
+
+const sequelize = new Sequelize(URL) 
 
 
 module.exports = sequelize;
